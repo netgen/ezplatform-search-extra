554d69986d7d36b6e8b03750157b2d8f4a70edf3
Douglas Hammond <wizhippo@gmail.com>
2019-01-24 16:25:52 -0500

    Add field facet builder

---
+++ lib/Core/Search/Solr/Query/Common/FacetBuilderVisitor/FieldFacetBuilderVisitor.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace Netgen\EzPlatformSearchExtra\Core\Search\Solr\Query\Common\FacetBuilderVisitor;
+
+use eZ\Publish\API\Repository\Values\Content\Query\Criterion;
+use eZ\Publish\API\Repository\Values\Content\Query\FacetBuilder;
+use eZ\Publish\API\Repository\Values\Content\Query\FacetBuilder\FieldFacetBuilder;
+use eZ\Publish\API\Repository\Values\Content\Search\Facet\FieldFacet;
+use eZ\Publish\Core\Search\Common\FieldNameResolver;
+use EzSystems\EzPlatformSolrSearchEngine\Query\FacetBuilderVisitor;
+use EzSystems\EzPlatformSolrSearchEngine\Query\FacetFieldVisitor;
+
+/**
+ * Visits the Field facet builder.
+ */
+class FieldFacetBuilderVisitor extends FacetBuilderVisitor implements FacetFieldVisitor
+{
+    /**
+     * @var FieldNameResolver
+     */
+    private $fieldNameResolver;
+
+    public function __construct(FieldNameResolver $fieldNameResolver)
+    {
+        $this->fieldNameResolver = $fieldNameResolver;
+    }
+
+    public function mapField($field, array $data, FacetBuilder $facetBuilder)
+    {
+        $values = [];
+        $totalCount = 0;
+        $missingCount = 0;
+
+        reset($data);
+        while ($key = current($data)) {
+            $totalCount += $values[$key] = next($data);
+            next($data);
+        }
+
+        if (current($data) === null) {
+            $totalCount += $missingCount = next($data);
+        }
+
+        return new FieldFacet([
+            'name' => $facetBuilder->name,
+            'entries' => $values,
+            'missingCount' => $missingCount,
+            'totalCount' => $totalCount,
+            'otherCount' => $totalCount - $missingCount,
+        ]);
+    }
+
+    public function canVisit(FacetBuilder $facetBuilder)
+    {
+        return $facetBuilder instanceof FieldFacetBuilder;
+    }
+
+    public function visitBuilder(FacetBuilder $facetBuilder, $fieldId)
+    {
+        $parameters = [];
+        $criteria = new Criterion\MatchAll();
+        $fieldPaths = $facetBuilder->fieldPaths;
+
+        $parts = explode(':', $fieldPaths);
+        if (count($parts) > 1) {
+            $contentTypeIdentifier = $parts[0];
+            $criteria = new Criterion\ContentTypeIdentifier($contentTypeIdentifier);
+            $fieldPaths = $parts[1];
+        }
+
+        $parts = explode('/', $fieldPaths);
+        $fieldDefinitionIdentifier = $parts[0];
+        $name = isset($parts[1]) ? $parts[1] : null;
+
+        $fieldTypes = $this->fieldNameResolver->getFieldTypes(
+            $criteria,
+            $fieldDefinitionIdentifier,
+            null,
+            $name
+        );
+
+        foreach ($fieldTypes as $fieldName => $fieldType) {
+            $parameters = array_merge($parameters, [
+                'facet.field' => "{!ex=dt key={$fieldId}}{$fieldName}",
+                "f.{$fieldName}.facet.limit" => $facetBuilder->limit,
+                "f.{$fieldName}.facet.mincount" => $facetBuilder->minCount,
+                "f.{$fieldName}.facet.sort" => $this->getSort($facetBuilder),
+                "f.{$fieldName}.facet.missing" => 'true',
+            ]);
+        }
+
+        return $parameters;
+    }
+
+    private function getSort(FieldFacetBuilder $facetBuilder)
+    {
+        switch ($facetBuilder->sort) {
+            case FieldFacetBuilder::COUNT_DESC:
+                return 'count';
+            case FieldFacetBuilder::TERM_ASC:
+                return 'index';
+        }
+
+        return 'index';
+    }
+}
