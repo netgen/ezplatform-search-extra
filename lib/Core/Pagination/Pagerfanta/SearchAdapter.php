43450c7938cb680ee35590b0b11d6a17f0009fa5
Douglas Hammond <wizhippo@gmail.com>
2019-01-24 22:42:30 -0500

    Add improved pagerfanta support

---
+++ lib/Core/Pagination/Pagerfanta/SearchAdapter.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Netgen\EzPlatformSearchExtra\Core\Pagination\Pagerfanta;
+
+use eZ\Publish\API\Repository\SearchService;
+use eZ\Publish\API\Repository\Values\Content\LocationQuery;
+use eZ\Publish\API\Repository\Values\Content\Query;
+
+class SearchAdapter extends BaseAdapter
+{
+    /**
+     * @var \eZ\Publish\API\Repository\SearchService
+     */
+    private $searchService;
+
+    public function __construct(Query $query, SearchService $searchService)
+    {
+        parent::__construct($query);
+        $this->searchService = $searchService;
+    }
+
+    protected function executeQuery(Query $query)
+    {
+        if ($query instanceof LocationQuery) {
+            return $this->searchService->findLocations($query);
+        }
+
+        return $this->searchService->findContent($query);
+    }
+}
---
+++ lib/Core/Pagination/Pagerfanta/SearchHandlerAdapter.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Netgen\EzPlatformSearchExtra\Core\Pagination\Pagerfanta;
+
+use eZ\Publish\API\Repository\Values\Content\LocationQuery;
+use eZ\Publish\API\Repository\Values\Content\Query;
+use eZ\Publish\SPI\Search\Handler as SearchHandlerInterface;
+
+class SearchHandlerAdapter extends BaseAdapter
+{
+    /**
+     * @var SearchHandlerInterface
+     */
+    private $searchHandler;
+
+    public function __construct(Query $query, SearchHandlerInterface $searchHandler)
+    {
+        parent::__construct($query);
+        $this->searchHandler = $searchHandler;
+    }
+
+    protected function executeQuery(Query $query)
+    {
+        if ($query instanceof LocationQuery) {
+            return $this->searchHandler->findLocations($query);
+        }
+
+        return $this->searchHandler->findContent($query);
+    }
+}
