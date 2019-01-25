b97d00b9d69ca07b48cc5c57403bf1a1f9d9a421
Douglas Hammond <wizhippo@gmail.com>
2019-01-24 22:42:30 -0500

    Add improved pagerfanta support

---
+++ lib/Core/Pagination/Pagerfanta/Slice.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace Netgen\EzPlatformSearchExtra\Core\Pagination\Pagerfanta;
+
+use ArrayAccess;
+use ArrayIterator;
+use eZ\Publish\API\Repository\Values\Content\Search\SearchHit;
+use IteratorAggregate;
+use RuntimeException;
+
+/**
+ * Implements IteratorAggregate with access to the array of the SearchHit instances
+ * and aggregated ArrayIterator over values contained in them.
+ */
+final class Slice implements IteratorAggregate, ArrayAccess
+{
+    /**
+     * @var \eZ\Publish\API\Repository\Values\Content\Search\SearchHit[]
+     */
+    private $searchHits;
+
+    public function __construct(array $searchHits)
+    {
+        $this->searchHits = $searchHits;
+    }
+
+    public function getSearchHits()
+    {
+        return $this->searchHits;
+    }
+
+    public function getIterator()
+    {
+        return new ArrayIterator(
+            array_map(
+                function (SearchHit $searchHit) {
+                    return $searchHit->valueObject;
+                },
+                $this->searchHits
+            )
+        );
+    }
+
+    public function offsetExists($offset)
+    {
+        return \array_key_exists($offset, $this->searchHits);
+    }
+
+    public function offsetGet($offset)
+    {
+        return $this->searchHits[$offset]->valueObject;
+    }
+
+    public function offsetSet($offset, $value)
+    {
+        throw new RuntimeException('Method ' . __METHOD__ . ' is not supported');
+    }
+
+    public function offsetUnset($offset)
+    {
+        throw new RuntimeException('Method ' . __METHOD__ . ' is not supported');
+    }
+}
