dade34f4bccc23e794001767217f27729b132a34
Randy Čupić <randy@netgen.hr>
2019-06-17 11:57:47 +0200

    Add method in full text spell check interface to get spell check query

--- lib/API/Values/Content/Query/Criterion/FulltextSpellcheck.php
+++ lib/API/Values/Content/Query/Criterion/FulltextSpellcheck.php
@@ -4,5 +4,10 @@ namespace  Netgen\EzPlatformSearchExtra\API\Values\Content\Query\Criterion;
 
 interface FulltextSpellcheck
 {
-
+    /**
+     * Gets query to be used for spell check.
+     *
+     * @return string
+     */
+    public function getSpellCheckQuery();
 }
