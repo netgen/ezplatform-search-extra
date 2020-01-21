2c7843b3ae52c0fcf8a423aaad534084723fe7f0
Randy Čupić <randy@netgen.hr>
2020-01-21 15:10:29 +0100

    Add configuration for spellcheck component through criterion

---
+++ lib/API/Values/Content/SpellcheckQuery.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Netgen\EzPlatformSearchExtra\API\Values\Content;
+
+class SpellcheckQuery
+{
+    /**
+     * This parameter specifies the query to spellcheck.
+     *
+     * @var string
+     */
+    public $query;
+
+    /**
+     * This parameter specifies the maximum number of suggestions
+     * that the spellchecker should return for a term.
+     *
+     * @var int
+     */
+    public $count;
+
+    /**
+     * Additional Solr spellcheck params as an array to be encoded with \json_encode().
+     *
+     * Example:
+     *
+     * ```php
+     *  $query->parameters = [
+     *      'build': true,
+     *      'reload' => true,
+     *      'onlyMorePopular' => false,
+     *      'maxResultsForSuggest' => 5,
+     *  ];
+     * ```
+     *
+     * @var array
+     */
+    public $parameters;
+}
