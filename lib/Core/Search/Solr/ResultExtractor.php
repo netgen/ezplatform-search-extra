a518db9ee1b2f97a116e79604e8a29782156582e
Randy Čupić <randy@netgen.hr>
2021-01-07 11:11:54 +0100

    NGSTACK-489 Extract extra fields from Solr document

--- lib/Core/Search/Solr/ResultExtractor.php
+++ lib/Core/Search/Solr/ResultExtractor.php
@@ -2,8 +2,13 @@
 
 namespace Netgen\EzPlatformSearchExtra\Core\Search\Solr;
 
+use eZ\Publish\API\Repository\Values\Content\Search\SearchResult;
 use EzSystems\EzPlatformSolrSearchEngine\ResultExtractor as BaseResultExtractor;
 use Netgen\EzPlatformSearchExtra\Core\Search\Solr\API\FacetBuilder\RawFacetBuilder;
+use eZ\Publish\API\Repository\Values\Content\Query;
+use Netgen\EzPlatformSearchExtra\API\Values\Content\Search\Query as ExtraQuery;
+use Netgen\EzPlatformSearchExtra\API\Values\Content\Search\LocationQuery as ExtraLocationQuery;
+use Netgen\EzPlatformSearchExtra\API\Values\Content\Search\SearchHit;
 
 /**
  * This DocumentMapper implementation adds support for handling RawFacetBuilders.
@@ -12,9 +17,22 @@ use Netgen\EzPlatformSearchExtra\Core\Search\Solr\API\FacetBuilder\RawFacetBuild
  */
 abstract class ResultExtractor Extends BaseResultExtractor
 {
-    public function extract($data, array $facetBuilders = [])
+    public function extract($data, array $facetBuilders = [], ?Query $query = null)
     {
         $searchResult = $this->extractSearchResult($data, $facetBuilders);
+
+        foreach ($searchResult->searchHits as $key => $searchHit) {
+            $searchResult->searchHits[$key] = new SearchHit(get_object_vars($searchHit));
+            $searchResult->searchHits[$key]->extraFields = [];
+
+            if ($query instanceof ExtraQuery || $query instanceof ExtraLocationQuery) {
+                $searchResult->searchHits[$key]->extraFields = $this->extractExtraFields(
+                    $data,
+                    $searchResult->searchHits[$key],
+                    $query->extraFields
+                );
+            }
+        }
 
         if (!isset($data->facets) || $data->facets->count === 0) {
             return $searchResult;
@@ -56,5 +74,27 @@ abstract class ResultExtractor Extends BaseResultExtractor
                 return $facetBuilder instanceof RawFacetBuilder;
             }
         );
+    }
+
+    /**
+     * @param mixed $data
+     * @param \Netgen\EzPlatformSearchExtra\API\Values\Content\Search\SearchHit $searchResult
+     * @param string[] $extraFields
+     */
+    private function extractExtraFields($data, SearchHit $searchHit, $extraFields)
+    {
+        $extractedExtraFields = [];
+        foreach ($data->response->docs as $doc) {
+            if ($doc->document_type_id === 'content' && $doc->content_id_id == $searchHit->valueObject->id
+            || $doc->document_type_id === 'location' && $doc->location_id_id == $searchHit->valueObject->mainLocationId) {
+                foreach ($extraFields as $extraField) {
+                    if (property_exists($doc, $extraField)) {
+                        $extractedExtraFields[$extraField] = $doc->{$extraField};
+                    }
+                }
+            }
+        }
+
+        return $extractedExtraFields;
     }
 }
