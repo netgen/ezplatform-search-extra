c9621a83d947344c56233acecc38d90b2e8b1910
Randy Čupić <randy@netgen.hr>
2021-01-19 16:48:43 +0100

    NGSTACK-489 Convert search hit to kernel search hit

--- tests/lib/Kernel/SearchServiceLocationTest.php
+++ tests/lib/Kernel/SearchServiceLocationTest.php
@@ -5,6 +5,8 @@ namespace Netgen\EzPlatformSearchExtra\Tests\Kernel;
 use eZ\Publish\API\Repository\Tests\SearchServiceLocationTest as KernelSearchServiceLocationTest;
 use eZ\Publish\API\Repository\Values\Content\LocationQuery;
 use eZ\Publish\API\Repository\Values\Content\Search\SearchResult as KernelSearchResult;
+use eZ\Publish\API\Repository\Values\Content\Search\SearchHit as KernelSearchHit;
+use Netgen\EzPlatformSearchExtra\API\Values\Content\Search\SearchHit;
 use Netgen\EzPlatformSearchExtra\API\Values\Content\Search\SearchResult;
 
 class SearchServiceLocationTest extends KernelSearchServiceLocationTest
@@ -34,14 +36,32 @@ class SearchServiceLocationTest extends KernelSearchServiceLocationTest
 
     private function mapToKernelSearchResult(SearchResult $data)
     {
+        $kernelSearchHits = [];
+
+        /** @var \Netgen\EzPlatformSearchExtra\API\Values\Content\Search\SearchHit $searchHit */
+        foreach ($data->searchHits as $searchHit) {
+            $kernelSearchHits[] = $this->mapToKernelSearchHit($searchHit);
+        }
+
         return new KernelSearchResult([
             'facets' => $data->facets,
-            'searchHits' => $data->searchHits,
+            'searchHits' => $kernelSearchHits,
             'spellSuggestion' => $data->spellSuggestion,
             'time' => $data->time,
             'timedOut' => $data->timedOut,
             'maxScore' => $data->maxScore,
             'totalCount' => $data->totalCount,
+        ]);
+    }
+
+    private function mapToKernelSearchHit(SearchHit $searchHit)
+    {
+        return new KernelSearchHit([
+            'valueObject' => $searchHit->valueObject,
+            'score' => $searchHit->score,
+            'index' => $searchHit->index,
+            'matchedTranslation' => $searchHit->matchedTranslation,
+            'highlight' => $searchHit->highlight,
         ]);
     }
 }
