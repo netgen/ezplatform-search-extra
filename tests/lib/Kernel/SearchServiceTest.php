31d370c8b822b396147dcfa376dcf14bddd5657b
Randy Čupić <randy@netgen.hr>
2021-01-19 17:15:17 +0100

    NGSTACK-489 Convert search hit to kernel search hit v2

--- tests/lib/Kernel/SearchServiceTest.php
+++ tests/lib/Kernel/SearchServiceTest.php
@@ -5,6 +5,8 @@ namespace Netgen\EzPlatformSearchExtra\Tests\Kernel;
 use eZ\Publish\API\Repository\Tests\SearchServiceTest as KernelSearchServiceTest;
 use eZ\Publish\API\Repository\Values\Content\Query;
 use eZ\Publish\API\Repository\Values\Content\Search\SearchResult as KernelSearchResult;
+use eZ\Publish\API\Repository\Values\Content\Search\SearchHit as KernelSearchHit;
+use Netgen\EzPlatformSearchExtra\API\Values\Content\Search\SearchHit;
 use Netgen\EzPlatformSearchExtra\API\Values\Content\Search\SearchResult;
 
 class SearchServiceTest extends KernelSearchServiceTest
@@ -32,14 +34,32 @@ class SearchServiceTest extends KernelSearchServiceTest
 
     private function mapToKernelSearchResult(SearchResult $data): KernelSearchResult
     {
+        $kernelSearchHits = [];
+
+        /** @var \Netgen\EzPlatformSearchExtra\API\Values\Content\Search\SearchHit $searchHit */
+        foreach ($data->searchHits as $searchHit) {
+            $kernelSearchHits[] = $this->mapToKernelSearchHit($searchHit);
+        }
+
         return new KernelSearchResult([
             'facets' => $data->facets,
-            'searchHits' => $data->searchHits,
+            'searchHits' => $kernelSearchHits,
             'spellSuggestion' => $data->spellSuggestion,
             'time' => $data->time,
             'timedOut' => $data->timedOut,
             'maxScore' => $data->maxScore,
             'totalCount' => $data->totalCount,
+        ]);
+    }
+
+    private function mapToKernelSearchHit(SearchHit $searchHit): KernelSearchHit
+    {
+        return new KernelSearchHit([
+            'valueObject' => $searchHit->valueObject,
+            'score' => $searchHit->score,
+            'index' => $searchHit->index,
+            'matchedTranslation' => $searchHit->matchedTranslation,
+            'highlight' => $searchHit->highlight,
         ]);
     }
 }
