1b6dd3fe3ebc3e071b7457b112c9202225da89f7
Randy Čupić <randy@netgen.hr>
2021-01-21 15:56:21 +0100

    NGSTACK-489 Implement content and location field mapper

---
+++ tests/lib/Integration/Implementation/Solr/FieldMapper/TestFieldMapperInterface.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Netgen\EzPlatformSearchExtra\Tests\Integration\Implementation\Solr\FieldMapper;
+
+interface TestFieldMapperInterface
+{
+    const CONTENT_TYPE_IDENTIFIER = 'extra_fields_test';
+
+    const CHILD_CONTENT_TYPE_IDENTIFIER = 'extra_fields_test_comment';
+}
